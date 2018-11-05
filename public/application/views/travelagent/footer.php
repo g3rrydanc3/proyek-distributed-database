8046f6458ed6c12ec18e28ed07b96026e4e14cab
chippeido <31821648+chippeido@users.noreply.github.com>
2018-11-05 08:45:46 +0700

    Revert "Travel Agent UI"
    This reverts commit a960dfc3b471bca123539a8499b0fbe0497e7d24.

--- public/application/views/travelagent/footer.php
+++ public/application/views/travelagent/footer.php
@@ -1,83 +1,11 @@
-<footer class="footer">
-		<div class="container">
-			<div class="row">
-
-				<!-- Footer Column -->
-				<div class="col-lg-4 footer_col">
-					<div class="footer_about">
-						<!-- Logo -->
-						<div class="logo_container">
-							<div class="logo">
-								<div>HOTEL DD</div>
-								<div>travel agency</div>
-								<div class="logo_image"><img src="<?php  echo base_url() ?>asset/images/logo.png" alt=""></div>
-							</div>
-						</div>
-						<div class="footer_about_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar sed mauris eget tincidunt. Sed lectus nulla, tempor vel eleifend quis, tempus rut rum metus. Pellentesque ultricies enim eu quam fermentum hendrerit.</div>
-						<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a target="_blank">PROYEK DD @STTS2018</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-					</div>
-				</div>
-
-				<!-- Footer Column -->
-				<div class="col-lg-4 footer_col">
-					<div class="footer_latest">
-						<div class="footer_title">Latest News</div>
-						<div class="footer_latest_content">
-
-							<!-- Footer Latest Post -->
-							<div class="footer_latest_item">
-								<div class="footer_latest_image"><img src="<?php  echo base_url() ?>asset/images/latest_1.jpg" alt="https://unsplash.com/@peecho"></div>
-								<div class="footer_latest_item_content">
-									<div class="footer_latest_item_title"><a href="news.html">Brazil Summer</a></div>
-									<div class="footer_latest_item_date">Jan 09, 2018</div>
-								</div>
-							</div>
-
-							<!-- Footer Latest Post -->
-							<div class="footer_latest_item">
-								<div class="footer_latest_image"><img src="<?php  echo base_url() ?>asset/images/latest_2.jpg" alt="https://unsplash.com/@sanfrancisco"></div>
-								<div class="footer_latest_item_content">
-									<div class="footer_latest_item_title"><a href="news.html">A perfect vacation</a></div>
-									<div class="footer_latest_item_date">Jan 09, 2018</div>
-								</div>
-							</div>
-
-						</div>
-					</div>
-				</div>
-
-				<!-- Footer Column -->
-				<div class="col-lg-4 footer_col">
-					<div class="tags footer_tags">
-						<div class="footer_title">Tags</div>
-						<ul class="tags_content d-flex flex-row flex-wrap align-items-start justify-content-start">
-							<li class="tag"><a href="#">travel</a></li>
-							<li class="tag"><a href="#">summer</a></li>
-							<li class="tag"><a href="#">cruise</a></li>
-							<li class="tag"><a href="#">beach</a></li>
-							<li class="tag"><a href="#">offer</a></li>
-							<li class="tag"><a href="#">vacation</a></li>
-							<li class="tag"><a href="#">trip</a></li>
-							<li class="tag"><a href="#">city break</a></li>
-							<li class="tag"><a href="#">adventure</a></li>
-						</ul>
-					</div>
-				</div>
-
-			</div>
-		</div>
-	</footer>
-</div>
-
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/js/jquery-3.2.1.min.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/css/bootstrap4/popper.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/css/bootstrap4/bootstrap.min.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/plugins/easing/easing.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/plugins/parallax-js-master/parallax.min.js"></script>
-<script src="<?php  echo base_url() ?><?php  echo base_url() ?>asset/js/custom.js"></script>
-</body>
-</html>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+    </div>
+    <!-- /.container -->
+    </footer>
+<?php $this->load->view('_footer'); ?>
