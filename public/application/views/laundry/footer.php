dfe5cce8161261885290126f8e5c01e252cfcd73
Gerry Surya <ger_26_6@hotmail.com>
2018-11-08 18:37:10 +0700

    Laundry template sb admin

--- public/application/views/laundry/footer.php
+++ public/application/views/laundry/footer.php
@@ -1,11 +1,39 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-    <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © <?= $hotel_name?> 2018</span>
+          </div>
+        </div>
+      </footer>
+
+    </div><!-- /.content-wrapper -->
+
+  </div><!-- /#wrapper -->
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
     </div>
-    <!-- /.container -->
-    </footer>
-<?php $this->load->view('_footer'); ?>
+  </div>
+
+</body>
+<?php $this->load->view('_load_global_js')?>
+
+</html>
--- public/application/views/laundry/home.php
+++ public/application/views/laundry/home.php
@@ -1,123 +1,94 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<?php $this->load->view("frontoffice/header") ?>
+<?php $this->load->view("laundry/header") ?>
 
-<!-- Page Content -->
-<div class="container">
+<div class="container-fluid">
 
-<div class="row">
-  <div class="col-sm-8">
-    <h2 class="mt-4">What We Do <i class="fas fa-camera-retro"></i></h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
-    <p>
-      <a class="btn btn-primary btn-lg" href="#">Call to Action &raquo;</a>
-    </p>
-  </div>
-  <div class="col-sm-4">
-    <h2 class="mt-4">Contact Us</h2>
-    <address>
-      <strong>Start Bootstrap</strong>
-      <br>3481 Melrose Place
-      <br>Beverly Hills, CA 90210
-      <br>
-    </address>
-    <address>
-      <abbr title="Phone">P:</abbr>
-      (123) 456-7890
-      <br>
-      <abbr title="Email">E:</abbr>
-      <a href="mailto:#">name@example.com</a>
-    </address>
-  </div>
-</div>
-<!-- /.row -->
+	<!-- Breadcrumbs-->
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item">
+			<a href="#">Dashboard</a>
+		</li>
+		<li class="breadcrumb-item active">Overview</li>
+	</ol>
 
-<div class="row">
-  <div class="col-sm-4 my-4">
-    <div class="card">
-      <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-      <div class="card-body">
-        <h4 class="card-title">Card title</h4>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-      </div>
-      <div class="card-footer">
-        <a href="#" class="btn btn-primary">Find Out More!</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4 my-4">
-    <div class="card">
-      <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-      <div class="card-body">
-        <h4 class="card-title">Card title</h4>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-      </div>
-      <div class="card-footer">
-        <a href="#" class="btn btn-primary">Find Out More!</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4 my-4">
-    <div class="card">
-      <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-      <div class="card-body">
-        <h4 class="card-title">Card title</h4>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-      </div>
-      <div class="card-footer">
-        <a href="#" class="btn btn-primary">Find Out More!</a>
-      </div>
-    </div>
-  </div>
+	<!-- Icon Cards-->
+	<div class="row">
+		<div class="col-xl-3 col-sm-6 mb-3">
+			<div class="card text-white bg-primary o-hidden h-100">
+				<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-comments"></i>
+					</div>
+					<div class="mr-5">26 New Messages!</div>
+				</div>
+				<a class="card-footer text-white clearfix small z-1" href="#">
+					<span class="float-left">View Details</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+				</a>
+			</div>
+		</div>
+		<div class="col-xl-3 col-sm-6 mb-3">
+			<div class="card text-white bg-warning o-hidden h-100">
+				<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-list"></i>
+					</div>
+					<div class="mr-5">11 New Tasks!</div>
+				</div>
+				<a class="card-footer text-white clearfix small z-1" href="#">
+					<span class="float-left">View Details</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+				</a>
+			</div>
+		</div>
+		<div class="col-xl-3 col-sm-6 mb-3">
+			<div class="card text-white bg-success o-hidden h-100">
+				<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-shopping-cart"></i>
+					</div>
+					<div class="mr-5">123 New Orders!</div>
+				</div>
+				<a class="card-footer text-white clearfix small z-1" href="#">
+					<span class="float-left">View Details</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+				</a>
+			</div>
+		</div>
+		<div class="col-xl-3 col-sm-6 mb-3">
+			<div class="card text-white bg-danger o-hidden h-100">
+				<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-life-ring"></i>
+					</div>
+					<div class="mr-5">13 New Tickets!</div>
+				</div>
+				<a class="card-footer text-white clearfix small z-1" href="#">
+					<span class="float-left">View Details</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+				</a>
+			</div>
+		</div>
+	</div>
 
-</div>
-<!-- /.row -->
+</div><!-- /.container-fluid -->
 
-<p class="currency">10000</p>
 
-<table id="example" class="table table-striped table-bordered" style="width:100%">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php for($i=0; $i<= 100; $i++):?>
-        <tr>
-            <td>Tiger Nixon</td>
-            <td>System Architect</td>
-            <td>Edinburgh</td>
-            <td>61</td>
-            <td>2011/04/25</td>
-            <td class="currency"><?= random_int(100, 1000000)?></td>
-        </tr>
-        <?php endfor;?>
-    </tbody>
-    <tfoot>
-        <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
-        </tr>
-    </tfoot>
-</table>
-
-</div>
-
-<?php $this->load->view("frontoffice/footer") ?>
+<?php $this->load->view("laundry/footer") ?>
 
 <script>
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+	$(document).ready(function () {
+
+	});
+
 </script>
