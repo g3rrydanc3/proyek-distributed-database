92c51454072589762ae49475d0b64d5f17a0d6f7
alfonsiaronna <alfonsiaronnalf@gmail.com>
2018-11-08 14:18:28 +0700

    DATABASE LAUNDRY

--- database/sql/laundry.sql
+++ database/sql/laundry.sql
@@ -1,13 +1,24 @@
+connect system/123@laundry
+
+drop user adminlaundry cascade;
+drop user employeelaundry cascade;
+
+create user adminlaundry identified by admin;
+grant connect, UNLIMITED TABLESPACE, resource to adminlaundry;
+
+create user employeelaundry identified by employee;
+grant connect to employeelaundry;
+
 -- -----------------------------------------------------
 -- Table employee
 -- -----------------------------------------------------
 CREATE TABLE employee (
-  employee_id NUMBER(10) NOT NULL,
-  first_name VARCHAR2(100) NOT NULL,
-  last_name VARCHAR2(100) NOT NULL,
-  username VARCHAR2(100) NOT NULL,
-  password VARCHAR2(100) NOT NULL,
-  PRIMARY KEY (employee_id),
+  employee_id NUMBER(10),
+  first_name VARCHAR2(100) CONSTRAINT NN_EMPLOYEE_FIRST_NAME NOT NULL,
+  last_name VARCHAR2(100) CONSTRAINT NN_EMPLOYEE_LAST_NAME NOT NULL,
+  username VARCHAR2(100) CONSTRAINT NN_EMPLOYEE_USERNAME NOT NULL,
+  password VARCHAR2(100) CONSTRAINT NN_EMPLOYEE_PASSWORD NOT NULL,
+  CONSTRAINT PK_EMPLOYEE_EMPLOYEE_ID PRIMARY KEY (employee_id),
   CONSTRAINT username_UNIQUE UNIQUE  (username))
 ;
 
@@ -16,14 +27,13 @@ CREATE TABLE employee (
 -- Table laundry_bill
 -- -----------------------------------------------------
 CREATE TABLE laundry_bill (
-  laundry_bill_id NUMBER(10) NOT NULL,
-  room_no NUMBER(10) NOT NULL,
-  employee_id NUMBER(10) NOT NULL,
-  total NUMBER(10) NOT NULL,
-  status NUMBER(3) NOT NULL,
-  bill_date TIMESTAMP NOT NULL,
-  PRIMARY KEY (laundry_bill_id)
-  CREATE INDEX fk_laundry_bill_employee_idx ON laundry_bill (employee_id ASC),
+  laundry_bill_id NUMBER(10),
+  room_no NUMBER(10) CONSTRAINT NN_LAUNDRY_BILL_ROOM_NO NOT NULL,
+  employee_id NUMBER(10) CONSTRAINT NN_LAUNDRY_BILL_EMPLOYEE_ID NOT NULL,
+  total NUMBER(10) CONSTRAINT NN_LAUNDRY_BILL_TOTAL NOT NULL,
+  status NUMBER(3) CONSTRAINT NN_LAUNDRY_BILL_STATUS NOT NULL,
+  bill_date TIMESTAMP CONSTRAINT NN_LAUNDRY_BILL_BILL_DATE NOT NULL,
+  CONSTRAINT PK_LAUNDRY_BILL_LAUNDRY_BILL_ID PRIMARY KEY (laundry_bill_id),
   CONSTRAINT fk_laundry_bill_employee
     FOREIGN KEY (employee_id)
     REFERENCES employee (employee_id)
@@ -35,10 +45,10 @@ CREATE TABLE laundry_bill (
 -- Table laundry_service
 -- -----------------------------------------------------
 CREATE TABLE laundry_service (
-  laundry_service_id NUMBER(10) NOT NULL,
-  service_type VARCHAR2(45) NOT NULL,
-  price NUMBER(10) NOT NULL,
-  PRIMARY KEY (laundry_service_id))
+  laundry_service_id NUMBER(10),
+  service_type VARCHAR2(45) CONSTRAINT NN_LAUNDRY_SERVICE_SERVICE_TYPE NOT NULL,
+  price NUMBER(10) CONSTRAINT NN_LAUNDRY_SERVICE_PRICE NOT NULL,
+  CONSTRAINT PK_LAUNDRY_SERVICE_LAUNDRY_SERVICE_ID PRIMARY KEY (laundry_service_id))
 ;
 
 
@@ -46,14 +56,12 @@ CREATE TABLE laundry_service (
 -- Table laundry_bill_detail
 -- -----------------------------------------------------
 CREATE TABLE laundry_bill_detail (
-  laundry_bill_detail_id NUMBER(10) NOT NULL,
-  laundry_bill_id NUMBER(10) NOT NULL,
-  laundry_service_id NUMBER(10) NOT NULL,
-  weight NUMBER(3) NOT NULL,
-  price NUMBER(10) NOT NULL,
-  PRIMARY KEY (laundry_bill_detail_id)
-  CREATE INDEX fk_laundry_bill_detail_laundry_bill1_idx ON laundry_bill_detail (laundry_bill_id ASC)
-  CREATE INDEX fk_laundry_bill_detail_laundry_service1_idx ON laundry_bill_detail (laundry_service_id ASC),
+  laundry_bill_detail_id NUMBER(10),
+  laundry_bill_id NUMBER(10) CONSTRAINT PK_LAUNDRY_BILL_DETAIL_LAUNDRY_BILL_ID NOT NULL,
+  laundry_service_id NUMBER(10) CONSTRAINT PK_LAUNDRY_BILL_DETAIL_LAUNDRY_SERVICE_ID NOT NULL,
+  weight NUMBER(3) CONSTRAINT PK_LAUNDRY_BILL_DETAIL_WEIGHT NOT NULL,
+  price NUMBER(10) CONSTRAINT PK_LAUNDRY_BILL_DETAIL_PRICE NOT NULL,
+  CONSTRAINT PK_LAUNDRY_BILL_DETAIL_LAUNDRY_BILL_DETAIL_ID PRIMARY KEY (laundry_bill_detail_id),
   CONSTRAINT fk_laundry_bill_detail_laundry_bill1
     FOREIGN KEY (laundry_bill_id)
     REFERENCES laundry_bill (laundry_bill_id)
